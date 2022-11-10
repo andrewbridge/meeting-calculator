9629fcbf475417a15aae316f4ab4c0baaafaf501
Andrew Bridge <andrew@andrewhbridge.co.uk>
2022-11-10 15:39:36 +0000

    feat: redirect user to configuration when no attendees exist

--- src/services/routes.mjs
+++ src/services/routes.mjs
@@ -1,6 +1,7 @@
 import { ref } from "../deps/vue.mjs";
 import Configure from "../pages/Configure.mjs";
 import Search from "../pages/Calculator.mjs";
+import { attendees } from "./data.mjs";
 
 export const routes = {
     '/': Search,
@@ -16,6 +17,7 @@ export const getRoute = (component) => '#' + (routeMap.get(component) || '/');
 
 export const activeHash = ref(window.location.hash.slice(1) || '/');
 export const activeRoute = ref(routes[activeHash.value]);
+if (attendees.value.length === 0) window.location.hash = '/configure';
 
 window.addEventListener('hashchange', () => {
     activeHash.value = window.location.hash.slice(1) || '/';
